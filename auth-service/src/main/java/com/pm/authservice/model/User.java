69bc9f9c4c3fe3cab2db4878c5cd718fa2c2b5cb
Samarth <tikotkarsamarth@gmail.com>
2025-07-03 03:01:39 +0530

    implement jwt auth and integrate to api gateway

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,43 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
